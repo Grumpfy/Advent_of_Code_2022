ec03d16fb39f8ade1a98c32fd6e90cf2535c8168
Stephane Guy <stephane.guy@allegorithmic.com>
2022-12-01 01:05:20 +0100

    add third parties

--- 00_test.cpp
+++ 00_test.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <fmt/core.h>
+#include <cppcoro/generator.hpp>
+#include <absl/container/flat_hash_map.h>
+
+auto fibonacci() -> cppcoro::generator<const int> {
+  int fib_k_0 = 0;
+  int fib_k_1 = 1;
+  while (true) {
+    co_yield fib_k_1;
+    fib_k_0 = std::exchange(fib_k_1, fib_k_0 + fib_k_1);
+  }
+}
 
 auto main() -> int {
-  std::cout << "hello world\n";
+  fmt::print("Hello, world!\n");
+  for (auto fib : fibonacci()) {
+    constexpr auto upper_bound = 10;
+    if (fib > upper_bound) {break;}
+    fmt::print("Fib: {}\n", fib);
+  }
+  auto absl_map = absl::flat_hash_map<int, int>{};
+  absl_map[3] = 2;
+  fmt::print("absl_map[{}]: {}\n", 3, absl_map[3]);
   return 0;
 }
